ae621295b29ce3fc1c4704e6cdc4f06b74ab23fc
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-11-20 16:08:00 +0100

    Add product component

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  @Input() productName: string = '';
+  @Output() productClicked = new EventEmitter();
+
+constructor(private productsService: ProductsService) {}
+
+  onClicked() {
+    //this.productClicked.emit();
+    this.productsService.deleteProduct(this.productName);
+  }
+}
