42dcf372a92ae2a0d373e7595656db4ff350d050
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-11-20 20:39:24 +0100

    Fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './product/product.component';
-import { ProductsService } from './products.service';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
-    ProductComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
