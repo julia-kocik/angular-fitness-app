70f44f138199727cb52f215f374f8fa717c87521
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-11-30 23:33:11 +0100

    Pass data via routing

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,10 +7,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  isPhonePortrait!: boolean;
   maxDate!: Date;
-  ngOnInit() {
+ngOnInit() {
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
+    this.isPhonePortrait = history.state.isPhonePortrait
+    console.log(this.isPhonePortrait)
   }
   onSubmit(form:NgForm) {
     console.log(form)
