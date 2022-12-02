f28bb538a9ef0cf2d0e4a562259d58430269de45
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-12-02 22:58:38 +0100

    Refactor code by adding Header and Sidenav-list component

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.css']
+})
+export class SidenavListComponent {
+  @Output() sidenavClose = new EventEmitter<void>()
+  onCloseSidenav() {
+    this.sidenavClose.emit();
+  }
+}
