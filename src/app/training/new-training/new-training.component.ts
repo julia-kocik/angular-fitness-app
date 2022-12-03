277be630b84280484e9ecdf5a61ffa30251ae650
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-12-03 15:50:19 +0100

    Add responsive service for

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { ResponsiveService } from 'src/app/responsive.service';
 
 @Component({
   selector: 'app-new-training',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent {
-
+  isPhonePortrait: boolean = false;
+  isTabletPortrait: boolean = false;
+  constructor(private responsive: BreakpointObserver, private responsiveService: ResponsiveService) {}
+  ngOnInit() {
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        const breakpoints = result.breakpoints;
+        const resultValues = this.responsiveService.checkDementions(breakpoints[Breakpoints.HandsetPortrait], breakpoints[Breakpoints.TabletPortrait])
+        this.isPhonePortrait = resultValues.isPhonePortrait
+        this.isTabletPortrait = resultValues.isTabletPortrait
+    });
+}
 }
