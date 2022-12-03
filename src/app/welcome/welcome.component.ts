6f4dc7bf254c924f6c79dd95a8ea8d3e35f1533a
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-12-03 11:43:51 +0100

    Add content for Welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+  isPhonePortrait!: boolean
+  isTabletPortrait!: boolean
 
+    constructor(private responsive: BreakpointObserver) {}
+  ngOnInit() {
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        const breakpoints = result.breakpoints;
+        if(!breakpoints[Breakpoints.HandsetPortrait] && !breakpoints[Breakpoints.TabletPortrait]) {
+          this.isTabletPortrait = false;
+          this.isPhonePortrait = false;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.isPhonePortrait = true;
+          this.isTabletPortrait = false;
+        } else if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.isTabletPortrait = true;
+          this.isPhonePortrait = false;
+        }
+      console.log(this.isPhonePortrait, this.isTabletPortrait)
+  });
+  }
 }
