0ed19c009015ecf71579d24cfa1a10fe70868cd8
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-12-05 13:23:16 +0100

    Add Training provider

---
+++ src/app/training/training.service.ts
@@ -0,0 +1,19 @@
+import { Exercise } from "./exercise.model";
+
+export class TrainingService {
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+  ]
+  private runningExercise: Exercise | undefined;
+
+  getAvailableExercises() {
+    return this.availableExercises;
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+  }
+}
