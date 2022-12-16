6a21c4d5e3e62411fc3f46e8dec3454daf59dcac
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-12-16 11:20:41 +0100

    Add auth routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { ReactiveFormsModule } from "@angular/forms";
-import { MaterialModule } from "../material.module";
 import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth-routing.module";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 
@@ -11,7 +11,8 @@ import { SignupComponent } from "./signup/signup.component";
     imports: [
         ReactiveFormsModule,
         AngularFireAuthModule,
-        SharedModule
+        SharedModule,
+        AuthRoutingModule
     ],
     exports: [],
 })
